3bc851d9acf4bc62754fa7fce420ef5a21d93e7d
Atul Chandra <atulchandra021@gmail.com>
2024-02-19 18:50:12 +0530

    Ep-06 fully completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,60 +4,90 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+  // local State Variable - super powerful variable
+  const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]);
 
-    // local State Variable - super powerful variable
-    const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
+  // whenever state variable update, react triggers a reconixa
+  console.log("Body rendered");
 
-    useEffect(()=>{
-      fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5976892&lng=88.46703219999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    const fetchData = async () =>{
-      const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5976892&lng=88.46703219999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      );
-
-      const json = await data.json();
-      console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-      setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-      
-
-    };
-    
-
-    // condition Rendering ---- Render according to condition is know as condition rendering
-  
-    if(ListOfRestaurants.length === 0){
-      return <Shimmer/>
-    }
-
-
-
-    return (
-      <div className="body">
-        <div className="body">
-            
-            <button 
-                className="filter-btn"  
-                onClick={()=>{
-                    const filteredList = ListOfRestaurants.filter((res)=>res.info.avgRating>4.5
-                    );
-                    setListOfRestaurants(filteredList);
-            
-                }}       
-            >
-            Top Rated restaurant</button>
-            
-        </div>
-        <div className="res-container">
-          {ListOfRestaurants.map((restaurant)=>(
-              <RestaurantCard key = {restaurant.info.id} resData = {restaurant}/>
-            ))}
-        </div>
-      </div>
+    const json = await data.json();
+    console.log(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestaurants(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setfilteredRestaurants(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
     );
   };
+
+  // condition Rendering ---- Render according to condition is know as condition rendering
+
+  if (ListOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // Filter  the restaurant card and update the UI
+              // SearchText
+              console.log(searchText);
+
+              const filteredRestaurant = ListOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              console.log(filteredRestaurant);
+              setfilteredRestaurants(filteredRestaurant);
+            }}
+          >
+            search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
