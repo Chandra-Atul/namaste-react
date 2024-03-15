9b117782e5086af9e4f227dcbf4674395f3bc8ca
Atul Chandra <atulchandra021@gmail.com>
2024-03-15 12:12:40 +0530

    Ep-08 part2 completed

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [count1] = useState(1);
+  const [count2] = useState(2);
+
   return (
     <div className="user-card">
+      <h1>count1 : {count1}</h1>
+      <h1>count2 : {count2}</h1>
+
       <h2>Name: {props.name}</h2>
       <h3>Location: {props.location}</h3>
       <h4>Contact: @atulchandra021</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,12 +3,20 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count1: 1,
+      count2: 2,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count1, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>count1: {count1}</h1>
+        <h1>count2: {count2}</h1>
+
         <h2>Name: {name}</h2>
         <h3>location: {location}</h3>
         <h4>Contact: @atulchandra021</h4>
